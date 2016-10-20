20b23a1f8a810425249b6201a07e3eac92b1eddf
Adam Sandberg Eriksson <adam@sandbergericsson.se>
2016-10-20 02:01:40 +0200

    Simple globby matching

---
+++ Glob.hs
@@ -0,0 +1,21 @@
+-- |
+
+module Glob where
+
+
+match pat str = or (match' pat str)
+
+match'
+  :: String -- ^ pattern
+  -> String -- ^ string
+  -> [Bool]
+match' [] [] = [True]
+match' ('*':pat) (_:cs) = match' pat cs ++ match' ('*':pat) cs
+match' (p:pat) (c:cs)
+  | p == c = match' pat cs
+  | otherwise = [False]
+match' _ _ = [False]
+
+examples = ["/Dir/Page", "/Dir/*", "*/Page"]
+
+test = map (flip match "/Dir/Page") examples
